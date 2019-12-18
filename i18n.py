6bd103da3db42ef1e86f1ae841246deca6f2f520
ghnoob <ptnchk.rodrigo@gmail.com>
2019-12-17 22:19:09 -0300

    Update i18n.py
    Now the app creates a cfg file with default values if the file does not exist or exists but there is no language option.

--- i18n.py
+++ i18n.py
@@ -8,9 +8,20 @@ import gettext
 import configparser
 
 def load_cfg():
-    """Loads the language configuration."""
+    """Loads the language configuration from config.cfg file.
+    
+    If the config file does not exist or it does not have a language
+    option, it creates a new cfg with default values.
+    """
     config = configparser.ConfigParser()
-    config.read('config.cfg')
+    
+    if (config.read('config.cfg') is False or
+    config.has_option('language', 'lang') is False):
+        #  create cfg with default values
+        config['language'] = {'lang':'os'}
+        with open('config.cfg', 'w') as configfile:
+            config.write(configfile)
+    
     return config.get('language', 'lang')
     
 def set_lang():
@@ -25,11 +36,13 @@ def set_lang():
     if l == 'en':
         lang = gettext.translation('base', fallback=True)
     elif l == 'es':
-        lang = gettext.translation('base', localedir='locales', languages=['es'])
+        lang = gettext.translation('base', localedir='locales',
+               languages=['es'])
     else:
         loc = locale.getlocale()
         if loc[0].startswith( ('Spanish', 'es') ):
-            lang = gettext.translation('base', localedir='locales', languages=['es'])
+            lang = gettext.translation('base', localedir='locales',
+                   languages=['es'])
         else:
             lang = gettext.translation('base', fallback=True)
     
